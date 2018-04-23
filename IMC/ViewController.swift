9ec90c9fbedade43ed4970a0a1329176c1025b35
Thiago Simoes Goncalves <thiago.goncalves@concrete.com.br>
2018-04-23 11:01:25 -0300

    finalizando o APP existem ainda alguns detalhes a resolver

--- IMC/ViewController.swift
+++ IMC/ViewController.swift
@@ -10,6 +10,17 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet weak var tf_weight: UITextField!
+    @IBOutlet weak var tf_height: UITextField!
+    @IBOutlet weak var lb_result: UILabel!
+    @IBOutlet weak var iv_result: UIImageView!
+    
+    @IBOutlet weak var vi_result: UIView!
+    
+    var imc: Double = 0
+    
+   
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -19,7 +30,42 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        view.endEditing(true)
+    }
 
-
+    @IBAction func calculate(_ sender: Any) {
+        if let weight = Double(tf_weight.text!), let height = Double(tf_height.text!){
+            imc = weight / pow(height,2)
+            showResults()
+        }
+    }
+    
+    func showResults(){
+        var result: String = ""
+        var image: String = ""
+        switch imc {
+            case 0..<16:
+                result = "Puro Osso"
+                image = "abaixo"
+            case 16..<18.5:
+                result = "Olivia Palito"
+                image = "magreza"
+            case 18.5..<25:
+                result = "Idealzinho de merda"
+                image = "ideal"
+            case 25..<30:
+                result = "Ta ficando grande em"
+                image = "sobre"
+            default:
+                result = "Chupeta de Baleia"
+                image = "obesidade"
+        }
+        lb_result.text = "\(Int(imc)): \(result)"
+        iv_result.image = UIImage(named: image)
+        vi_result.isHidden = false
+        view.endEditing(true)
+    }
+    
 }
 
